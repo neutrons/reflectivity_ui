deef93b41b7fb6c210e41cb79aff399017bbfcdc
Mathieu Doucet <doucetm@ornl.gov>
2018-06-15 14:44:07 -0400

    pylint: data_info.py

--- reflectivity_ui/interfaces/data_handling/data_info.py
+++ reflectivity_ui/interfaces/data_handling/data_info.py
@@ -1,13 +1,13 @@
 """
     Meta-data information for MR reduction
 """
+#pylint: disable=too-few-public-methods
 from __future__ import absolute_import, division, print_function
 import sys
 
 # Import mantid according to the application configuration
 from . import ApplicationConfiguration
-application_conf = ApplicationConfiguration()
-sys.path.insert(0, application_conf.mantid_path)
+sys.path.insert(0, ApplicationConfiguration().mantid_path)
 from mantid.simpleapi import *
 
 
@@ -27,12 +27,12 @@ class DataInfo(object):
                       ForcePeakROI=configuration.force_peak_roi, PeakROI=configuration.peak_roi,
                       ForceLowResPeakROI=configuration.force_low_res_roi, LowResPeakROI=configuration.low_res_roi,
                       ForceBckROI=configuration.force_bck_roi, BckROI=configuration.bck_roi)
-                 
+
         self.cross_section = cross_section
         self.run_number = ws.getRunNumber()
 
         run_object = ws.getRun()
-        self.is_direct_beam = run_object.getProperty("is_direct_beam").value.lower()=='true'
+        self.is_direct_beam = run_object.getProperty("is_direct_beam").value.lower() == 'true'
         self.data_type = 0 if self.is_direct_beam else 1
         #if ws.getNumberEvents() < self.n_events_cutoff:
         #    self.data_type = -1
@@ -40,14 +40,14 @@ class DataInfo(object):
         # Processing options
         # Use the ROI rather than finding the ranges
         self.use_roi = configuration.use_roi
-        self.use_roi_actual = run_object.getProperty("use_roi_actual").value.lower()=='true'
+        self.use_roi_actual = run_object.getProperty("use_roi_actual").value.lower() == 'true'
 
         self.calculated_scattering_angle = run_object.getProperty("calculated_scatt_angle").value
 
         tof_min = run_object.getProperty("tof_range_min").value
         tof_max = run_object.getProperty("tof_range_max").value
         self.tof_range = [tof_min, tof_max]
-        
+
         peak_min = run_object.getProperty("peak_min").value
         peak_max = run_object.getProperty("peak_max").value
         self.peak_range = [peak_min, peak_max]
@@ -56,7 +56,7 @@ class DataInfo(object):
         background_min = run_object.getProperty("background_min").value
         background_max = run_object.getProperty("background_max").value
         self.background = [background_min, background_max]
-        
+
         low_res_min = run_object.getProperty("low_res_min").value
         low_res_max = run_object.getProperty("low_res_max").value
         self.low_res_range = [low_res_min, low_res_max]
