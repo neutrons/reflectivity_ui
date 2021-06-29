e36910ef8690baad4f3c1ec83fc42a433070564f
Jose Borreguero <borreguero@gmail.com>
2021-06-29 13:54:24 -0400

    Check for preferred mantid installation and its location. Also unit test
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- reflectivity_ui/interfaces/data_handling/__init__.py
+++ reflectivity_ui/interfaces/data_handling/__init__.py
@@ -1,9 +1,18 @@
 """
     Data handling using Mantid
 """
+
+import os
+
+
 class ApplicationConfiguration(object):
-    """
-        Application-level configuration
+    r"""Application-level configuration
+    1. Selection of the mantid version and installation location:
+        - Version is preferred in the following order, from most to least preferred: ['4.2.0', '4.1.0', '4.0.0']
+        - Preferred installation location is under /opt, e.g. "/opt/mantid42". If no preferred installation is found,
+          then a mantid installation is searched in the locations specified by `sys.path`.
+          NOTE: the preferred installation is not tested for integrity. Only the existence of the location is tested,
+          but not its contents.
     """
     # Polarization states
     POL_STATE = "PolarizerState"
@@ -11,5 +20,23 @@ class ApplicationConfiguration(object):
     ANA_STATE = "AnalyzerState"
     ANA_VETO = "AnalyzerVeto"
 
-    def __init__(self):
-        self.mantid_path = '/opt/mantid40'
+    _valid_mantid_versions = ['4.2.0', '4.1.0', '4.0.0']
+
+    def __init__(self, root_dir='/opt'):
+        self.mantid_path = None
+        self.mantid_version = None
+        # Check for installed Mantid versions under /opt
+        for version in self._valid_mantid_versions:
+            short = ''.join(version.split('.')[0:2])
+            install_path = os.path.join(root_dir, 'mantid' + short)
+            if os.path.isdir(install_path):  # check only for the existence of the directory, but not its contents
+                self.mantid_path = install_path
+                self.mantid_version = version
+                return
+        # Check for installed Mantid versions already in the PYTHONPATH
+        try:
+            import mantid
+            self.mantid_path = os.path.dirname(mantid.__file__)
+            self.mantid_version = mantid.__version__
+        except ImportError:
+            pass
