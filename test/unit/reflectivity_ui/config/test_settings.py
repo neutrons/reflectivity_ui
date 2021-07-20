e7b625647553be28eb536719122078a51b40ec63
Jose Borreguero <borreguero@gmail.com>
2021-07-20 05:50:58 -0400

    UI settings singleton
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

---
+++ test/unit/reflectivity_ui/config/test_settings.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# local imports
+from reflectivity_ui.config import Settings
+
+# 3rd-party imports
+import pytest
+
+
+class TestConfiguration(object):
+    def test_init(self):
+        settings = Settings()
+
+    def test_singleton(self):
+        settings1 = Settings()
+        settings2 = Settings()
+        assert id(settings1) == id(settings2)
+
+    def test_defaults(self):
+        settings = Settings()
+        assert settings['OpenSum']['LogNames'][-1] == 'S4Vheight'
+        assert settings['OpenSum']['Tolerances'][-1] == 0.01
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
