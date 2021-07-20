e202c47c7fcadb7b03e3d42c9118c399813e7230
Jose Borreguero <borreguero@gmail.com>
2021-07-20 15:59:59 -0400

    script to fetch remote files
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- test/conftest.py
+++ test/conftest.py
@@ -7,29 +7,10 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import pytest
 
 # standard imports
-import json
 import os
 import sys
-import urllib
 
 this_module_path = sys.modules[__name__].__file__
-
-
-#@pytest.fixture(scope="session", autouse=True)
-@pytest.mark.skip(reason="is this preventing Travis from testing?")
-def fetch_remote_files():
-    r"""Fetch remote files before any test runs"""
-    data_dir = os.path.join(os.path.dirname(this_module_path), 'data')
-    remote_info_file = os.path.join(data_dir, 'remote_files.json')
-    remote_info = json.load(open(remote_info_file, 'r'))
-
-    remote_address = remote_info['address']
-    remote_files = remote_info['files']
-
-    for basename, md5 in remote_files.items():
-        file_path = os.path.join(data_dir, basename)
-        if not os.path.isfile(file_path):
-            urllib.urlretrieve(os.path.join(remote_address, md5), file_path)
 
 
 @pytest.fixture(scope='module')
