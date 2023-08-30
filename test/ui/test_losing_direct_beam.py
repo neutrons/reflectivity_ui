6851197d5d73a62b962d2ba042fb58e52ddbc142
Marie Backman <backmanma@ornl.gov>
2023-08-30 13:26:55 -0400

    To fix defect where direct beams are lost, remove statement that accidentally assigned the direct beam to None for the currently active run and cross-section when another run is loaded

---
+++ test/ui/test_losing_direct_beam.py
@@ -0,0 +1,57 @@
+# local imports
+from reflectivity_ui.interfaces.main_window import MainWindow
+from test import SNS_REFM_MOUNTED
+from test.ui import ui_utilities
+
+# third party imports
+import pytest
+
+# standard library imports
+
+
+@pytest.mark.skipif(not SNS_REFM_MOUNTED, reason="/SNS/REF_M/ is not mounted")
+def test_losing_direct_beam(qtbot):
+    r"""Test that reduction list runs do not lose their direct beam when the user clicks another run in the file list"""
+    main_window = MainWindow()
+    qtbot.addWidget(main_window)
+
+    def _get_reduction_list_run_direct_beam(reduction_list_index, xs):
+        r"""Get the direct beam for the given reduction list run and cross-section
+        Returns
+        -------
+        int
+            the run number of the direct beam
+        """
+        return (
+            main_window.data_manager.reduction_list[reduction_list_index]
+            .cross_sections[xs]
+            .configuration.normalization
+        )
+
+    direct_beam_run = 40786
+
+    # load file list
+    ui_utilities.setText(main_window.numberSearchEntry, str(direct_beam_run), press_enter=True)
+
+    # select run in the file list
+    ui_utilities.set_current_file_by_run_number(main_window, direct_beam_run)
+    # add run to direct beams
+    main_window.actionNorm.triggered.emit()
+
+    # select run in the file list
+    ui_utilities.set_current_file_by_run_number(main_window, 40785)
+    # add run to the reduction list
+    main_window.actionAddPlot.triggered.emit()
+
+    assert _get_reduction_list_run_direct_beam(0, "On_Off") == direct_beam_run
+    assert _get_reduction_list_run_direct_beam(0, "Off_Off") == direct_beam_run
+
+    # select different run in the file list
+    ui_utilities.set_current_file_by_run_number(main_window, 40782)
+
+    assert _get_reduction_list_run_direct_beam(0, "On_Off") == direct_beam_run
+    assert _get_reduction_list_run_direct_beam(0, "Off_Off") == direct_beam_run
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
